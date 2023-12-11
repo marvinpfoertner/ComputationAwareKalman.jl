d117bb4970baebc19725636ee39c7b917d0a7eba
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2023-12-11 18:07:51 +0100

    `truncate` in library

--- src/filter.jl
+++ src/filter.jl
@@ -91,6 +91,20 @@ function update(
     )
 end
 
+function truncate(
+    M::TM,
+    svd_cutoff::T = 1e-12,
+) where {T<:AbstractFloat,TM<:AbstractMatrix{T}}
+    U, S, _ = svd(M)
+
+    trunc_idx = findlast(S .> svd_cutoff)
+    if isnothing(trunc_idx)
+        trunc_idx = 0
+    end
+
+    return U[:, 1:trunc_idx] * Diagonal(S[1:trunc_idx])
+end
+
 struct FilterCache{
     T<:AbstractFloat,
     Tm<:AbstractVector{T},
