17ef09e84843827fb4bc0c0efb52d6402c39cf8a
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2023-12-11 17:05:52 +0100

    push!(FilterCache)

--- src/filter.jl
+++ src/filter.jl
@@ -22,9 +22,13 @@ end
 
 struct FilterCache{
     T<:AbstractFloat,
+    Tlgssm<:AbstractDiscreteLGSSM,
     Tm<:AbstractVector{T},
     TM<:AbstractMatrix{T},
-    Tlgssm<:AbstractDiscreteLGSSM,
+    TM⁺<:AbstractMatrix{T},
+    TU<:AbstractMatrix{T},
+    Tw<:AbstractVector{T},
+    TW<:AbstractMatrix{T},
 }
     lgssm::Tlgssm
 
@@ -33,18 +37,45 @@ struct FilterCache{
 
     is::Vector{Int64}  # Number of PLS iterations
 
-    M⁺s::Vector{TM}  # Truncated downdate to covariance of update filter belief
+    M⁺s::Vector{TM⁺}  # Truncated downdate to covariance of update filter belief
 
-    Us::Vector{TM}
+    Us::Vector{TU}
 
-    ws::Vector{Tm}  # Hₖᵀuₖ
-    Ws::Vector{TM}  # HₖᵀUₖ
+    ws::Vector{Tw}  # Hₖᵀuₖ
+    Ws::Vector{TW}  # HₖᵀUₖ
 end
 
-function FilterCache{T,Tm,TM}(lgssm::Tlgssm) where {T,Tm,TM,Tlgssm}
-    return FilterCache{T,Tm,TM,Tlgssm}(lgssm, Tm[], TM[], Int64[], TM[], TM[], Tm[], TM[])
+function FilterCache{T,Tlgssm,Tm,TM,TM⁺,TU,Tw,TW}(
+    lgssm::Tlgssm,
+) where {T,Tlgssm,Tm,TM,TM⁺,TU,Tw,TW}
+    return FilterCache{T,Tlgssm,Tm,TM,TM⁺,TU,Tw,TW}(
+        lgssm,
+        Tm[],
+        TM[],
+        Int64[],
+        TM⁺[],
+        TU[],
+        Tw[],
+        TW[],
+    )
 end
 
 function FilterCache{T}(lgssm::Tlgssm) where {T,Tlgssm}
-    return FilterCache{T,Vector{T},Matrix{T}}(lgssm)
+    return FilterCache{T,Tlgssm,Vector{T},Matrix{T},Matrix{T},Matrix{T},Vector{T},Matrix{T}}(
+        lgssm,
+    )
+end
+
+function Base.push!(
+    fcache::FilterCache{T,Tlgssm,Tm,TM,TM⁺,TU,Tw,TW},
+    x::UpdatedBelief{T,Tm,TΣ,TM,TU,Tw,TW},
+    M⁺::TM⁺,
+) where {T,Tlgssm,Tm,TM,TM⁺,TU,Tw,TW,TΣ}
+    push!(fcache.ms, x.m)
+    push!(fcache.Ms, x.M)
+    push!(fcache.is, x.i)
+    push!(fcache.M⁺s, M⁺)
+    push!(fcache.Us, x.U)
+    push!(fcache.ws, x.w)
+    push!(fcache.Ws, x.W)
 end
