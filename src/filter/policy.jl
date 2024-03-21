954dae6513e2381a54dfe17d2529f9350b83626b
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-03-21 20:02:25 +0100

    Add `CoordinatePolicy`

--- src/filter/policy.jl
+++ src/filter/policy.jl
@@ -4,6 +4,20 @@ function (policy::CGPolicy)(; r::AbstractVector{<:AbstractFloat}, kwargs...)
     return r
 end
 
+struct CoordinatePolicy
+    idcs::Vector{Int}
+end
+
+function (policy::RandomGaussianPolicy)(;
+    i::Integer,
+    r::AbstractVector{<:AbstractFloat},
+    kwargs...,
+)
+    v = zeros(Bool, size(r))
+    v[i] = 1
+    return v
+end
+
 struct RandomGaussianPolicy{Trng<:Random.AbstractRNG}
     rng::Trng
 end
