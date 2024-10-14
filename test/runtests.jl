3fdf9b10d0a96dcd862f0c6cac2f19fea042962a
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-10-14 18:59:53 +0200

    Register `Aqua.jl` as a proper `@testset`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,11 @@
+using Test
 using ComputationAwareKalman
 using Aqua
 
-Aqua.test_all(ComputationAwareKalman, ambiguities = false, deps_compat = false)
+@testset "Aqua.jl" begin
+    Aqua.test_all(
+        ComputationAwareKalman;
+        ambiguities = (broken = true,),
+        deps_compat = (broken = true, check_weakdeps = (broken = true,)),
+    )
+end
