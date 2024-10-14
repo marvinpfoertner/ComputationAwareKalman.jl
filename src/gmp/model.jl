41c4b4ecdb549c53b641b1975b34d3a4a4eca46d
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-10-14 22:35:43 +0200

    Wider type signatures in model interface

--- src/gmp/model.jl
+++ src/gmp/model.jl
@@ -1,33 +1,26 @@
 abstract type AbstractGaussMarkovProcess end
 
-function μ(gmp::Tgmp, t::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:AbstractFloat} end
+function μ(gmp::Tgmp, t::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real} end
 
-function Σ(gmp::Tgmp, t::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:AbstractFloat} end
+function Σ(gmp::Tgmp, t::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real} end
 
-function lsqrt_Σ(
-    gmp::Tgmp,
-    t::Tt,
-) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:AbstractFloat}
+function lsqrt_Σ(gmp::Tgmp, t::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real}
     return sqrt(Σ(gmp, t))
 end
 
-function A(
-    gmp::Tgmp,
-    t::Tt,
-    s::Tt,
-) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:AbstractFloat} end
+function A(gmp::Tgmp, t::Tt, s::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real} end
 
 function A_b_lsqrt_Q(
     gmp::Tgmp,
     t::Tt,
     s::Tt,
-) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:AbstractFloat} end
+) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real} end
 
 function Base.rand(
     rng::Trng,
     gmp::Tgmp,
     t::Tt,
-) where {Trng<:Random.AbstractRNG,Tgmp<:AbstractGaussMarkovProcess,Tt<:AbstractFloat}
+) where {Trng<:Random.AbstractRNG,Tgmp<:AbstractGaussMarkovProcess,Tt<:Real}
     lsqrt_Σₜ = lsqrt_Σ(gmp, t)
 
     return μ(gmp, t) + lsqrt_Σₜ * randn(rng, eltype(lsqrt_Σₜ), size(lsqrt_Σₜ, 2))
@@ -42,7 +35,7 @@ function Base.rand(
 ) where {
     Trng<:Random.AbstractRNG,
     Tgmp<:AbstractGaussMarkovProcess,
-    Tt<:AbstractFloat,
+    Tt<:Real,
     Txₛ<:AbstractVector,
 }
     Aₜₛ, bₜₛ, lsqrt_Qₜₛ = A_b_lsqrt_Q(gmp, t, s)
