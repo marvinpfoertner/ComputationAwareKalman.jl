15f2c26e232c87d3cd034a0e97c9567ef75dcef2
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-10-14 23:50:28 +0200

    Add docstrings to model definition

--- src/gmp/model.jl
+++ src/gmp/model.jl
@@ -1,43 +1,72 @@
 abstract type AbstractGaussMarkovProcess end
 
-function μ(gmp::Tgmp, t::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real} end
+"""
+    μ(gmp::AbstractGaussMarkovProcess, t::Real)
 
-function Σ(gmp::Tgmp, t::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real} end
+Mean of the state at time `t`.
+"""
+μ(::AbstractGaussMarkovProcess, ::Real)
 
-function lsqrt_Σ(gmp::Tgmp, t::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real}
+"""
+    Σ(gmp::AbstractGaussMarkovProcess, t::Real)
+
+Covariance matrix of the state at time `t`.
+"""
+Σ(::AbstractGaussMarkovProcess, ::Real)
+
+"""
+    lsqrt_Σ(gmp::AbstractGaussMarkovProcess, t::Real)
+
+A left square root of the covariance matrix of the state at time `t`.
+"""
+function lsqrt_Σ(gmp::AbstractGaussMarkovProcess, t::Real)
     return sqrt(Σ(gmp, t))
 end
 
-function A(gmp::Tgmp, t::Tt, s::Tt) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real} end
+"""
+    A(gmp::AbstractGaussMarkovProcess, t::Real, s::Real)
 
-function A_b_lsqrt_Q(
-    gmp::Tgmp,
-    t::Tt,
-    s::Tt,
-) where {Tgmp<:AbstractGaussMarkovProcess,Tt<:Real} end
+Transition matrix from time `s` to time `t`.
 
-function Base.rand(
-    rng::Trng,
-    gmp::Tgmp,
-    t::Tt,
-) where {Trng<:Random.AbstractRNG,Tgmp<:AbstractGaussMarkovProcess,Tt<:Real}
+Must return the identity matrix for `t = s`.
+"""
+A(::AbstractGaussMarkovProcess, ::Real, ::Real)
+
+"""
+    A_b_lsqrt_Q(gmp::AbstractGaussMarkovProcess, t::Real, s::Real)
+
+Transition matrix, bias vector, and left square-root of the process noise covariance from time `s` to time `t`.
+
+Will only be called with `t >= s`.
+"""
+A_b_lsqrt_Q(::AbstractGaussMarkovProcess, ::Real, ::Real)
+
+"""
+    rand(rng::Random.AbstractRNG, gmp::AbstractGaussMarkovProcess, t::Real)
+
+Sample from the state at time `t`.
+"""
+function Base.rand(rng::Random.AbstractRNG, gmp::AbstractGaussMarkovProcess, t::Real)
     lsqrt_Σₜ = lsqrt_Σ(gmp, t)
 
     return μ(gmp, t) + lsqrt_Σₜ * randn(rng, eltype(lsqrt_Σₜ), size(lsqrt_Σₜ, 2))
 end
 
+"""
+    rand(rng::Random.AbstractRNG, gmp::AbstractGaussMarkovProcess, t::Real, s::Real, xₛ::AbstractVector)
+
+Sample from the state at time `t` given the state at time `s`.
+
+Will only be called with `t >= s`.
+Must return `xₛ` if `t == s`.
+"""
 function Base.rand(
-    rng::Trng,
-    gmp::Tgmp,
-    t::Tt,
-    s::Tt,
-    xₛ::Txₛ,
-) where {
-    Trng<:Random.AbstractRNG,
-    Tgmp<:AbstractGaussMarkovProcess,
-    Tt<:Real,
-    Txₛ<:AbstractVector,
-}
+    rng::Random.AbstractRNG,
+    gmp::AbstractGaussMarkovProcess,
+    t::Real,
+    s::Real,
+    xₛ::AbstractVector,
+)
     Aₜₛ, bₜₛ, lsqrt_Qₜₛ = A_b_lsqrt_Q(gmp, t, s)
 
     return Aₜₛ * xₛ + bₜₛ + lsqrt_Qₜₛ * randn(rng, eltype(lsqrt_Qₜₛ), size(lsqrt_Qₜₛ, 2))
--- src/model/dynamics.jl
+++ src/model/dynamics.jl
@@ -1,44 +1,89 @@
 abstract type AbstractGaussMarkovChain end
 
-# TODO: Document
-# function Base.length(gmc::Tgmc) where {Tgmc<:AbstractGaussMarkovChain} end
+"""
+    Base.length(gmc::AbstractGaussMarkovChain)
 
-function μ(gmc::Tgmc, k::Tk) where {Tgmc<:AbstractGaussMarkovChain,Tk<:Integer} end
+Number of states in the Gauss-Markov chain.
+"""
+Base.length(::AbstractGaussMarkovChain)
 
-function Σ(gmc::Tgmc, k::Tk) where {Tgmc<:AbstractGaussMarkovChain,Tk<:Integer} end
+# Mean
+function μ end
 
-function lsqrt_Σ(gmc::Tgmc, k::Tk) where {Tgmc<:AbstractGaussMarkovChain,Tk<:Integer}
+@doc raw"""
+    μ(gmc::AbstractGaussMarkovChain, k::Integer)
+
+Mean of the state at time step `k`.
+"""
+μ(::AbstractGaussMarkovChain, ::Integer)
+
+# Covariance matrix
+function Σ end
+
+@doc raw"""
+    Σ(gmc::AbstractGaussMarkovChain, k::Integer)
+
+Covariance matrix of the state at time step `k`.
+"""
+Σ(::AbstractGaussMarkovChain, ::Integer)
+
+@doc raw"""
+    lsqrt_Σ(gmc::AbstractGaussMarkovChain, k::Integer)
+
+A left square root of the covariance matrix of the state at time step `k`.
+"""
+function lsqrt_Σ(gmc::AbstractGaussMarkovChain, k::Integer)
     return sqrt(Σ(gmc, k))
 end
 
-# TODO: Document that this must return the identity for k = 0
-function A(gmc::Tgmc, k::Tk) where {Tgmc<:AbstractGaussMarkovChain,Tk<:Integer} end
+# Transition matrix
+function A end
 
-# TODO: Document that this will only be called with k >= 1
-function A_b_lsqrt_Q(gmc::Tgmc, k::Tk) where {Tgmc<:AbstractGaussMarkovChain,Tk<:Integer} end
+@doc raw"""
+    A(gmc::AbstractGaussMarkovChain, k::Integer)
 
-function Base.rand(
-    rng::Trng,
-    gmc::Tgmc,
-    k::Tk,
-) where {Trng<:Random.AbstractRNG,Tgmc<:AbstractGaussMarkovChain,Tk<:Integer}
+Transition matrix from time step `k` to time step `k + 1`.
+
+Must return the identity matrix for `k = 0`.
+"""
+A(::AbstractGaussMarkovChain, ::Integer)
+
+# Transition model
+function A_b_lsqrt_Q end
+
+@doc raw"""
+    A_b_lsqrt_Q(gmc::AbstractGaussMarkovChain, k::Integer)
+
+Transition matrix, bias vector, and left square-root of the process noise covariance from time step `k` to time step `k + 1`.
+
+Will only be called with `k >= 1`.
+"""
+A_b_lsqrt_Q(::AbstractGaussMarkovChain, ::Integer)
+
+"""
+    rand(rng::Random.AbstractRNG, gmc::AbstractGaussMarkovChain, k::Integer)
+
+Sample from the state at time step `k`.
+"""
+function Base.rand(rng::Random.AbstractRNG, gmc::AbstractGaussMarkovChain, k::Integer)
     lsqrt_Σₖ = lsqrt_Σ(gmc, k)
 
     return μ(gmc, k) + lsqrt_Σₖ * randn(rng, eltype(lsqrt_Σₖ), size(lsqrt_Σₖ, 2))
 end
 
-# TODO: Document that this must return xₖ for k = 0
+"""
+    rand(rng::Random.AbstractRNG, gmc::AbstractGaussMarkovChain, k::Integer, xₖ::AbstractVector)
+
+Sample from the state at time step `k` given the value of the state at time step `k - 1`.
+
+Must return `xₖ` for `k = 0`.
+"""
 function Base.rand(
-    rng::Trng,
-    gmc::Tgmc,
-    k::Tk,
-    xₖ::Txₖ,
-) where {
-    Trng<:Random.AbstractRNG,
-    Tgmc<:AbstractGaussMarkovChain,
-    Tk<:Integer,
-    Txₖ<:AbstractVector,
-}
+    rng::Random.AbstractRNG,
+    gmc::AbstractGaussMarkovChain,
+    k::Integer,
+    xₖ::AbstractVector,
+)
     if k == 0
         return xₖ
     end
